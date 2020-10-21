4a4369584169a04040adfdc47f29dc494e05f926
Ndagi Stanley <ndagis@gmail.com>
2020-10-21 23:05:55 +0300

    Create Blog model

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
-import "fmt"
-func main(){
-  fmt.Println("Good to Go!")
+import (
+	"fmt"
+)
+func main() {
+	blog := New()
+	fmt.Println(blog)
+	blog.SaveArticle(Article{"My first Blog post", "Today, we will be talking about blogging"})
+	fmt.Println(blog)
 }
